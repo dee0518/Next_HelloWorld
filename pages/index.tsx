f35e805adda134460ff02ce3bcf41e84d3c46101
harseille <joonhabaak@gmail.com>
2023-03-01 00:21:12 +0900

    :bug: 파일 대소문자 변경
    Co-authored-by: choi1five <cwo1401@gmail.com>
    Co-authored-by: dee0518 <dehya0518@naver.com>
    Co-authored-by: davinahi <kdbj12003@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import type { NextPage } from 'next';
-import ModalContainer from '@/components/common/Modal/ModalContainer';
+// import ModalContainer from '@/components/common/Modal/ModalContainer';
 
 const Home: NextPage = () => {
-  return <ModalContainer />;
+  // return <ModalContainer />;
+  return <div>제발</div>;
 };
 
 export default Home;
