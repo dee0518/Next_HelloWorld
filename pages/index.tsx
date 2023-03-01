de6b2828ac3aadf589bfd84cf3d67be672e40080
harseille <joonhabaak@gmail.com>
2023-03-01 22:20:34 +0900

    :sparkles: 무중단 배포 테스트
    Co-authored-by: choi1five <cwo1401@gmail.com>
    Co-authored-by: dee0518 <dehya0518@naver.com>
    Co-authored-by: davinahi <kdbj12003@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   // return <ModalContainer />;
-  return <div>제발</div>;
+  return <div>무중단 배포 되고 있나요</div>;
 };
 
 export default Home;
